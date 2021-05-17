67457a0d2b013f5c2642d2c8d5ee2af00ffd9aad
LP <luizpaulobborges@gmail.com>
2021-05-17 15:05:13 -0300

    added user id field into meals table migration

---
+++ priv/repo/migrations/20210517175717_add_user_id_field_into_meals_table.exs
@@ -0,0 +1,9 @@
+defmodule ExdailyMeals.Repo.Migrations.AddUserIdFieldIntoMealsTable do
+  use Ecto.Migration
+
+  def change do
+    alter table(:meals) do
+      add :user_id, references(:users, type: :binary_id)
+    end
+  end
+end
